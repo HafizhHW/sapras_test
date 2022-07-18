91b296e1adcb77f23188b40494abc0360c7283b2
unknown <hafizzwijaya@gmail.com>
2022-07-18 20:55:23 +0700

    update testing

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -31,89 +31,111 @@ class Testing extends CI_Controller
     $this->load->view('Testing/V_Edit_testing', $data);
   }
 
+
+  // === BERASIL ===
   // public function add_act()
   // {
+  //   // $this->_rules();
+  //   // $dokumen = $this->Testing_model->dokumen($id);
+  //   // if ($this->form_validation->run() == FALSE) {
+  //   //   $this->add_test();
+  //   // } else {
+  //   $config = [
+  //     'upload_path' => 'assets/File',
+  //     'allowed_types' => 'jpg|jpeg',
+  //     'max_size' => 1028,
+  //     'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
+  //   ];
   //   $judull = $this->input->post('judul');
   //   $doct = $_FILES['doc']['name'];
-  //   if ($doct = ('')) {
-  //   } {
-  //     $config = [
-  //       'upload_path' => 'assets/File',
-  //       'allowed_types' => 'pdf|jpg|jpeg|png',
-  //       'max_size' => 1028,
-  //       'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
-  //     ];
+  //   if ($doct = 'doc') {
+
+
   //     $this->load->library('upload');
   //     $this->upload->initialize($config);
 
-
-  //     if (!$this->upload->do_upload('doc')) {
-  //       $error = array('error' => $this->upload->display_errors());
-  //       // var_dump($error);
-  //       // die;
-  //       $this->load->view('Testing/V_Add_testing', $error);
-  //     } else {
-  //       $doct = $this->upload->data();
-  //       $dokumen = $doct['file_name'];
+  //     if ($this->upload->do_upload('doc')) {
   //       $judull = $this->input->post('judul', TRUE);
-
+  //       $doct = $this->upload->data('file_name');
+  //       $data = [
+  //         'judul' => $judull,
+  //         'doc' => $doct
+  //       ];
+  //       $this->Testing_model->insert_data($data, 'files');
+  //       redirect('Testing');
+  //     } elseif ($doct = '') {
+  //       $judull = $this->input->post('judul', TRUE);
+  //       $doct = $this->upload->data('file_name');
   //       $data = array(
   //         'judul' => $judull,
-  //         'doc' => $dokumen
+  //         'doc' => $doct
   //       );
-  //       $this->db->insert('files', $data);
-  //       $this->session->set_flashdata('pesan', '
-  //     <div class="alert alert-success alert-dismissible fade show" role="alert">
-  //       Data Berhasil Ditambahkan !
-  //       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  //         <span aria-hidden="true">&times;</span>
-  //       </button>
-  //     </div>');
-
-  //       redirect('Testing/index');
+  //       $this->Testing_model->insert_data($data, 'files');
+  //       // $error = array('error' => $this->upload->display_errors());
+  //       // $this->load->view('Testing/V_Add_testing', $error);
+  //       $error = array('error' => $this->upload->display_errors());
+  //       $this->session->set_flashdata($error);
+  //       redirect('Testing/add_test');
+  //     } else {
+  //       $judull = $this->input->post('judul', TRUE);
+  //       // $doct = $this->upload->data('file_name');
+  //       $data = array(
+  //         'judul' => $judull,
+  //         // 'doc' => $doct
+  //       );
+  //       $this->Testing_model->insert_data($data, 'files');
+  //       redirect('Testing');
   //     }
   //   }
   // }
 
-  //=== BERHASIL ===
-  // public function add_act()
-  // {
-  //   $config = [
-  //     'upload_path' => 'assets/File',
-  //     'allowed_types' => 'pdf',
-  //     'max_size' => 1028,
-  //     'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
-  //   ];
-  //   $this->load->library('upload');
-  //   $this->upload->initialize($config);
-
-
-  //   if (!$this->upload->do_upload('doc')) {
-  //     $error = array('error' => $this->upload->display_errors());
-  //     // var_dump($error);
-  //     // die;
-  //     $this->load->view('Testing/V_Add_testing', $error);
-  //   } else {
-  //     $dokumen = $this->upload->data();
-  //     $dokumen = $dokumen['file_name'];
-  //     $judul = $this->input->post('judul', TRUE);
-
-  //     $data = array(
-  //       'judul' => $judul,
-  //       'doc' => $dokumen
-  //     );
-  //     $this->db->insert('files', $data);
-  //     $this->session->set_flashdata('pesan', '
-  //     <div class="alert alert-success alert-dismissible fade show" role="alert">
-  //       Data Berhasil Ditambahkan !
-  //       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  //         <span aria-hidden="true">&times;</span>
-  //       </button>
-  //     </div>');
-
-  //     redirect('Testing/index');
-  //   }
-  // }
+  function add_act()
+  {
+    $config = [
+      'upload_path' => 'assets/File',
+      'allowed_types' => 'jpg|jpeg',
+      'max_size' => 13000,
+      'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
+    ];
+    $judull = $this->input->post('judul');
+    $doct = $_FILES['doc']['name'];
+    //$ext = $_FILES['doc']['file_ext'];
+    //
+    $dname = explode(".", $_FILES['doc']['name']);
+    $ext = end($dname);
+    //
+    //print_r($ext);
+    //exit;
+    if ($doct != "") {
+      if ($ext == "jpg" || $ext == "jpeg" || $ext == "png" || $ext == "JPG") {
+        $judull = $this->input->post('judul', TRUE);
+        $doct = $this->upload->data('file_name');
+        $data = array(
+          'judul' => $judull,
+          'doc' => $doct
+        );
+        $this->Testing_model->insert_data($data, 'files');
+        $error = array('error' => $this->upload->display_errors());
+        $this->session->set_flashdata($error);
+        redirect('Testing/add_test');
+      } else {
+        echo '<script>
+						alert("File yang diupload Harus JPG!");
+						window.location.assign("../Testing/add_test");
+				</script>';
+        exit;
+      }
+    } else {
+      $judull = $this->input->post('judul', TRUE);
+      // $doct = $this->upload->data('file_name');
+      $data = array(
+        'judul' => $judull,
+        // 'doc' => $doct
+      );
+      $this->Testing_model->insert_data($data, 'files');
+      redirect('Testing');
+    }
+  }
 
   public function edit_act($id)
   {
@@ -230,75 +252,7 @@ class Testing extends CI_Controller
   //   }
   // }
 
-  // public function add_act2()
-  // {
-  //   // $this->_rules();
-  //   // $dokumen = $this->Testing_model->dokumen($id);
-  //   // if ($this->form_validation->run() == FALSE) {
-  //   //   $this->add_test();
-  //   // } else {
-  //   $config = [
-  //     'upload_path' => 'assets/File',
-  //     'allowed_types' => '*',
-  //     'max_size' => 1028,
-  //     'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
-  //   ];
 
-  //   $this->load->library('upload');
-  //   $this->upload->initialize($config);
-  //   if (!$this->upload->do_upload('doc')) {
-  //     // var_dump($this->upload->display_errors());
-  //     // die();
-  //     echo "Gagal Upload";
-  //   } else {
-  //     $upload_data = $this->upload->data();
-  //     $name = $upload_data['file_name'];
-
-  //     $insert = $this->Testing_model->insert_doc($name);
-  //     if ($insert) {
-  //       redirect('Testing');
-  //     }
-  //     echo "Gagal";
-  //   }
-  // }
-
-  // public function edit_act2()
-  // {
-  //   $id = $this->input->post('id');
-  //   $data = $this->Testing_model->getDataById($id);
-  //   $nama = 'assets/File/' . $data->doc;
-
-  //   if (is_readable($nama) && unlink($nama)) {
-  //     $config = [
-  //       'upload_path' => 'assets/File',
-  //       'allowed_types' => '*',
-  //       'max_size' => 1028,
-  //       'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
-  //     ];
-
-  //     $this->load->library('upload');
-  //     $this->upload->initialize($config);
-  //     if (!$this->upload->do_upload('doc')) {
-  //       // var_dump($this->upload->display_errors());
-  //       // die();
-  //       echo "Gagal Upload";
-  //     } else {
-  //       $upload_data = $this->upload->data();
-  //       $name = $upload_data['file_name'];
-
-  //       $data = array(
-  //         'judul' => $this->input->post('judul'),
-  //         'doc' => $name
-  //       );
-  //       $update = $this->Testing_model->updateFile($id, $data);
-  //       if ($update) {
-  //         redirect('Testing');
-  //       } else {
-  //         echo "Gagal";
-  //       }
-  //     }
-  //   }
-  // }
 
   // public function hapusFile($id)
   // {
@@ -313,80 +267,10 @@ class Testing extends CI_Controller
   //   }
   // }
 
-  public function add_act()
-  {
-    // $this->_rules();
-    // $dokumen = $this->Testing_model->dokumen($id);
-    // if ($this->form_validation->run() == FALSE) {
-    //   $this->add_test();
-    // } else {
-    $config = [
-      'upload_path' => 'assets/File',
-      'allowed_types' => 'jpg|jpeg',
-      'max_size' => 1028,
-      'file_name' => 'item-' . date('ymd') . '-' . substr(md5(rand()), 0, 10)
-    ];
-    $judull = $this->input->post('judul');
-    $doct = $_FILES['doc']['name'];
-    if ($doct = 'doc') {
-
-
-      $this->load->library('upload');
-      $this->upload->initialize($config);
-
-      if ($this->upload->do_upload('doc')) {
-        $judull = $this->input->post('judul', TRUE);
-        $doct = $this->upload->data('file_name');
-        $data = [
-          'judul' => $judull,
-          'doc' => $doct
-        ];
-        $this->Testing_model->insert_data($data, 'files');
-        redirect('Testing');
-      } elseif ($doct = '') {
-        $judull = $this->input->post('judul', TRUE);
-        $doct = $this->upload->data('file_name');
-        $data = array(
-          'judul' => $judull,
-          'doc' => $doct
-        );
-        $this->Testing_model->insert_data($data, 'files');
-        // $error = array('error' => $this->upload->display_errors());
-        // $this->load->view('Testing/V_Add_testing', $error);
-        $error = array('error' => $this->upload->display_errors());
-        $this->session->set_flashdata($error);
-        redirect('Testing/add_test');
-      } else {
-        $judull = $this->input->post('judul', TRUE);
-        // $doct = $this->upload->data('file_name');
-        $data = array(
-          'judul' => $judull,
-          // 'doc' => $doct
-        );
-        $this->Testing_model->insert_data($data, 'files');
-        redirect('Testing');
-      }
-    }
 
 
 
-    // $error = array('error' => $this->upload->display_errors());
-    // $this->load->view('Testing/V_Add_testing', $error);
 
-    // $error = array('error' => $this->upload->display_errors());
-    // $this->session->set_flashdata($error);
-
-    // $this->session->set_flashdata('pesan', '
-    //   <div class="alert alert-success alert-dismissible fade show" role="alert">
-    //     Data Berhasil Ditambahkan !
-    //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    //       <span aria-hidden="true">&times;</span>
-    //     </button>
-    //   </div>');
-
-    // redirect('Testing');
-    // }
-  }
 
 
   public function _rules()
